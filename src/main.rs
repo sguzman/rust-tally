723f398bd4133a1125a910705c4406f01483abff
Salvador Guzman <guzmansalv@gmail.com>
2020-06-21 09:49:06 -0700

    working chunk code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 use std::fs;
 use std::env;
 
-pub fn chunk(str: String, n: u8) -> Vec<String> {
+pub fn chunk(str: String, n: usize) -> Vec<String> {
     let vecky: Vec<String> = vec![String::from("stub")];
+    let partition: Vec<String> = {
+        let mut partition: Vec<String> = Vec::new();
+        let limit: usize = str.len() - n;
 
-    return vecky;
+        for i in 0..limit {
+            let part: String = str.chars().skip(i).take(n).collect();
+            partition.push(part);
+        }
+
+        partition
+    };
+
+    partition
 }
 
 fn main() {
     for argument in env::args() {
         println!("Found {}", argument);
-        let out: Vec<String> = chunk(argument, 2);
+        let out: Vec<String> = chunk(argument, 3);
+        for o in out {
+            println!("\t{}", o);
+        }
     }
 
     println!("Hello, world!");
