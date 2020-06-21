2db96ce94145ae718b744e7417beb86d0c840520
Salvador Guzman <guzmansalv@gmail.com>
2020-06-21 08:59:53 -0700

    chunky test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs;
+use std::env;
+
+pub fn chunk(str: String, n: u8) -> Vec<String> {
+    let vecky: Vec<String> = vec![String::from("stub")];
+
+    return vecky;
+}
+
 fn main() {
+    for argument in env::args() {
+        println!("Found {}", argument);
+        let out: Vec<String> = chunk(argument, 2);
+    }
+
     println!("Hello, world!");
 }
